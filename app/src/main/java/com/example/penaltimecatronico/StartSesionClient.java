9ca7dbc74a4e8df74062fe0594772bde1dc3390c
Domenica Velasco <dlvelasc@espol.edu.ec>
2020-12-20 19:21:32 -0500

    Creacion de actividades

---
+++ app/src/main/java/com/example/penaltimecatronico/StartSesionClient.java
@@ -0,0 +1,4 @@
+package com.example.penaltimecatronico;
+
+public class StartSesionClient {
+}
---
+++ app/src/main/java/com/example/penaltimecatronico/StartSesionRegister.java
@@ -0,0 +1,14 @@
+package com.example.penaltimecatronico;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+
+public class StartSesionRegister {
+
+}
